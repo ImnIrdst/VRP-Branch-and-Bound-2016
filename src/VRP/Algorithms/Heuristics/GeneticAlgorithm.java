5799b8700b0d8214ed1a672151da173c7397f6bf
Iman Irandoost <imn.irdst@gmail.com>
2016-07-27 12:41:18 +0430

    Fill in the run function in Genetic Algorithm class

--- src/VRP/Algorithms/Heuristics/GeneticAlgorithm.java
+++ src/VRP/Algorithms/Heuristics/GeneticAlgorithm.java
@@ -1,39 +1,82 @@
 package VRP.Algorithms.Heuristics;
 
+import VRP.Graph.Graph;
+
+import java.util.*;
+
 /**
  * An Implementation of GA Algorithm used for
  * calculating an upper bound for our problem (VRPD)
  * Created by iman on 7/27/16.
  */
 public class GeneticAlgorithm {
-    private double minimumCost;
 
-    private Chromosome[] population;
+    private Graph graph;
+    private int vehicleQty;
+    private int customerQty;
+    private int populationSize;
+    private double minimumCost;
+    private List<Chromosome> population;
+
+    private long startTime;
+
+    private final double mutationProbability = 0.001;
 
     /**
      * Constructor Creates a population with given qty
      */
-    public GeneticAlgorithm(int customerQty, int vehiclesQty, int populationQty) {
-        population = new Chromosome[populationQty];
-        for (int i = 0; i < populationQty; i++) population[i] = new Chromosome(customerQty + vehiclesQty);
-
-        initializePopulation(customerQty, vehiclesQty);
+    public GeneticAlgorithm(Graph graph, int customerQty, int vehicleQty, int populationSize) {
+        this.graph = graph;
+        this.vehicleQty = vehicleQty;
+        this.customerQty = customerQty;
+        this.populationSize = populationSize;
+        this.minimumCost = Double.MAX_VALUE;
+        this.population = new ArrayList<>();
+        for (int i = 0; i < populationSize; i++)
+            population.add(new Chromosome(customerQty + vehicleQty));
     }
 
     /**
      * run The algorithm to the given time
      *
-     * @param computeDurationMileSecond is how much time can be consumed
+     * @param computeDurationMilliSecond is how much time can be consumed
      */
-    public void run(int computeDurationMileSecond) {
+    public void run(int computeDurationMilliSecond) {
+        startTime = System.currentTimeMillis();
+        initializePopulation(customerQty, vehicleQty);
 
+        while (System.currentTimeMillis() > startTime + computeDurationMilliSecond) {
+            List<Chromosome> newPopulation = new ArrayList<>(population);
+
+            // cross over
+            for (Chromosome c1 : population) {
+                for (Chromosome c2 : population) newPopulation.add(crossOver(c1, c2));
+            }
+
+            // mutate
+            for (Chromosome c1 : population) mutate(c1);
+
+            // selection
+            population = selection(newPopulation);
+
+            // update best answer
+            minimumCost = population.get(0).getCost();
+        }
     }
 
     /**
      * initializes the population by shuffling the ids of nodes
      */
-    public void initializePopulation(int numberOfCustomers, int numberOfVehicles) {
+    public void initializePopulation(int customerQty, int vehicleQty) {
 
+        for (int i = 0; i < customerQty + vehicleQty; i++) {
+            if (i < customerQty)
+                population.get(i).set(i, i);
+            else
+                population.get(i).set(i, customerQty);
+
+            population.get(i).shuffle(); // shuffle to generate a random population
+        }
     }
 
     /**
@@ -41,7 +84,7 @@ public class GeneticAlgorithm {
      *
      * @param chromosome array of id of the nodes
      */
-    public void mutate(int[] chromosome) {
+    public void mutate(Chromosome chromosome) {
 
     }
 
@@ -52,7 +95,11 @@ public class GeneticAlgorithm {
      * @param chromosome2 array of id of the nodes
      * @return a new chromosome by crossover of two given chromosomes
      */
-    public int[] crossOver(int[] chromosome1, int[] chromosome2) {
+    public Chromosome crossOver(Chromosome chromosome1, Chromosome chromosome2) {
+        return null;
+    }
+
+    public List<Chromosome> selection(List<Chromosome> chromosomes) {
         return null;
     }
 
@@ -60,7 +107,7 @@ public class GeneticAlgorithm {
      * @param chromosome array of id of the nodes
      * @return the cost for given chromosome
      */
-    public double fitnessFunction(int[] chromosome) {
+    public double fitnessFunction(Chromosome chromosome) {
         return 0;
     }
 
@@ -71,12 +118,28 @@ public class GeneticAlgorithm {
         return minimumCost;
     }
 
+    /**
+     * Chromosome class for a set of ids (for VRPD) problem
+     */
     private class Chromosome implements Comparable<Chromosome> {
-        private int size;
-        private int[] ids;
+        public int size;
+        public List<Integer> list;
 
         public Chromosome(int size) {
-            ids = new int[size];
+            list = new ArrayList<>(size);
+        }
+
+        public Chromosome(Chromosome chromosome) {
+            this.size = chromosome.size;
+            this.list = new ArrayList<>(chromosome.list);
+        }
+
+        public int get(int idx) {
+            return list.get(idx);
+        }
+
+        public void set(int idx, int value) {
+            list.set(idx, value);
         }
 
         public int getCost() {
@@ -86,6 +149,10 @@ public class GeneticAlgorithm {
         @Override
         public int compareTo(Chromosome o) {
             return Double.compare(this.getCost(), o.getCost());
+        }
+
+        public void shuffle() {
+            Collections.shuffle(list);
         }
     }
 }
